ce58d4289df0ee4f57dc93be1297401a24191575
Shinelon <Shinelon@atguigu.com>
2020-03-18 20:36:06 +0800

    项目搭建日志分析

---
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/service/impl/ProductAttributeCategoryServiceImpl.java
@@ -0,0 +1,22 @@
+package com.atguigu.gmall.pms.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.atguigu.gmall.pms.entity.ProductAttributeCategory;
+import com.atguigu.gmall.pms.mapper.ProductAttributeCategoryMapper;
+import com.atguigu.gmall.pms.service.ProductAttributeCategoryService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>
+ * 产品属性分类表 服务实现类
+ * </p>
+ *
+ * @author Lfy
+ * @since 2020-03-12
+ */
+@Component
+@Service
+public class ProductAttributeCategoryServiceImpl extends ServiceImpl<ProductAttributeCategoryMapper, ProductAttributeCategory> implements ProductAttributeCategoryService {
+
+}
